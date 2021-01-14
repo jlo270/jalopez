3018969061df2c20b339da9501e157dc8195da24
jlo270 <lopez.jason270@gmail.com>
2021-01-13 18:35:35 -0600

    Added hero container and basic header

--- jalopez/src/app/app.module.ts
+++ jalopez/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { HeroComponent } from './hero/hero.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
